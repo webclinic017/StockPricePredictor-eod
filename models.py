078914b0f9934f09deb70f1227ccf075ec8adcd3
Filip Dvorak <filipdvrk@gmail.com>
2023-06-13 23:31:38 +0200

    added chatgpt sentiment analysis

--- models.py
+++ models.py
@@ -19,3 +19,22 @@ def general_model(features: int):
     ])
     return model
 
+def hpq_model(features: int):
+    model = tf.keras.models.Sequential([
+
+        tf.keras.layers.Conv1D(filters=1, kernel_size=10,
+                               strides=1, padding="same",
+                               activation=tf.nn.selu,
+                               input_shape=[None, features]),
+
+        tf.keras.layers.Bidirectional(
+        tf.keras.layers.LSTM(3, return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(3)),
+        tf.keras.layers.Dense(4, activation=tf.nn.selu),
+        tf.keras.layers.Dense(3, activation=tf.nn.selu),
+        tf.keras.layers.Dense(2, activation=tf.nn.selu),
+        tf.keras.layers.Dense(2, activation=tf.nn.gelu),
+        tf.keras.layers.Dense(3, activation=tf.nn.gelu),            
+        tf.keras.layers.Dense(1, activation=tf.nn.relu),
+    ])
+    return model
